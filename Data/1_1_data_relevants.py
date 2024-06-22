56df41da6faf114d6394225dd9bb699e7216cbd4
Renan Ruan <renan.rrfs@gmail.com>
2024-06-22 17:00:29 -0300

    Melhor resultado para SVM

--- Data/1_1_data_relevants.py
+++ Data/1_1_data_relevants.py
@@ -3,7 +3,14 @@
 # id77 Alpha : Orientação de propagação do sinal em relação a bolha de plasma no eixo horizontal
 # id78 Gamma : Orientação de propagação do sinal em relação a bolha de plasma no eixo vertical
 # id16 Phi60l1 : 
-# id10 S4 : Indice de Amplitude da Cintilação
+
+
+### Restrições
+
+# id8 Elev : Elevação do satélite > 12
+# id10 S4 : Indice de Amplitude da Cintilação > 0.3
+# id70 HTL : Hora de Tempo Local entre  18 e 27
+
 
 ### Atributos Meta
 
